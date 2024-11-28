bb88ede88546929b3d68d5327a383519fd31f2ca
FranoJurkovic <jurkovicfrano2000@gmail.com>
2024-11-28 17:33:42 +0100

    17.0

--- src/firebaseconfing.ts
+++ src/firebaseconfing.ts
@@ -5,14 +5,28 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxfO-MAOz8EAvZu4KY7yeD6fxN95IQjLs",
-  authDomain: "todo-e553d.firebaseapp.com",
-  projectId: "todo-e553d",
-  storageBucket: "todo-e553d.firebasestorage.app",
-  messagingSenderId: "726812610191",
-  appId: "1:726812610191:web:4a576537ed6ceefa46eafa",
-  measurementId: "G-V7Q0RN58SF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+if (
+  !firebaseConfig.apiKey ||
+  !firebaseConfig.authDomain ||
+  !firebaseConfig.projectId ||
+  !firebaseConfig.storageBucket ||
+  !firebaseConfig.messagingSenderId ||
+  !firebaseConfig.appId ||
+  !firebaseConfig.measurementId
+) {
+  console.error("FirebaseConfing ne radi.");
+} else {
+  console.log("FirebaseConfing radi.");
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
